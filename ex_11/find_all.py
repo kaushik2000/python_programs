aa71e28c7ea2a60bcac709fdc6870cc7a303d12f
kaushik2000ksk <kaushik2000ksk@gmail.com>
2020-07-26 13:00:15 +0530

    modified:   find_all.py

--- ex_11/find_all.py
+++ ex_11/find_all.py
@@ -20,7 +20,11 @@ print(z) # ['5', '3', '0']
 z = re.findall('[A-Z0-9]', x)
 print(z) # ['F', 'U', 'G', 'M', 'G', 'M', 'T', '5', '3', '0']
 
-x = 'From : Using xyz@abc.com: Good Morning'
+x = 'From : Using xyz.org@abc.com : Good Morning'
 # Find all strings starting with 'F' followed by non-white-space characters, one or more in number, ending with ':'
 y = re.findall('^F\S+:', x)
 print(y) # []
+
+# Fine-tuning string extraction
+y = re.findall('\S+@\S+', x)
+print(y) # ['xyz.org@abc.com']
