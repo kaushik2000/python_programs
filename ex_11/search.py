79a0ba7148e7580d3634a7faefceb887807e33f1
kaushik2000ksk <kaushik2000ksk@gmail.com>
2020-07-26 12:17:19 +0530

    modified:   RegEx-RegularExpressions 	new file:   search.py

---
+++ ex_11/search.py
@@ -0,0 +1,17 @@
+# search.py
+import re
+
+x = 'From: Using xyz@abc.com: Good Morning'
+# Search for strings starting with 'F' followed by any characters, zero or more in number, ending with ':'
+y = re.search('^F.*:', x)
+print(y) # <re.Match object; span=(0, 24), match='From: Using xyz@abc.com:'> The match is greedy
+y = re.search('^F.*?:', x)
+print(y) # <re.Match object; span=(0, 24), match='From: Using xyz@abc.com:'> The match is greedy
+
+z = re.search('^f.*:', x)
+print(z) # None
+
+x = 'From : Using xyz@abc.com: Good Morning'
+# Search for strings starting with 'F' followed by any characters except white-space , one or more in number, ending with ':'
+y = re.search('^F\S+:', x)
+print(y) # None
