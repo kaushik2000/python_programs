4c4d7255fb396e88a1fb9e45517d4b717610bc40
kaushik2000ksk <kaushik2000ksk@gmail.com>
2020-07-26 12:25:15 +0530

    modified:   search.py

--- ex_11/search.py
+++ ex_11/search.py
@@ -6,12 +6,12 @@ x = 'From: Using xyz@abc.com: Good Morning'
 y = re.search('^F.*:', x)
 print(y) # <re.Match object; span=(0, 24), match='From: Using xyz@abc.com:'> The match is greedy
 y = re.search('^F.*?:', x)
-print(y) # <re.Match object; span=(0, 24), match='From: Using xyz@abc.com:'> The match is greedy
+print(y) # <re.Match object; span=(0, 24), match='From:'> The match is non-greedy
 
 z = re.search('^f.*:', x)
 print(z) # None
 
 x = 'From : Using xyz@abc.com: Good Morning'
-# Search for strings starting with 'F' followed by any characters except white-space , one or more in number, ending with ':'
+# Search for strings starting with 'F' followed by non-white-space characters, one or more in number, ending with ':'
 y = re.search('^F\S+:', x)
 print(y) # None
