c291a8f4d47b98c4c48a0c8eec79a9ac1a176bd4
kaushik2000ksk <kaushik2000ksk@gmail.com>
2020-07-29 13:12:43 +0530

    modified:   request_response.py

--- ex_12/request_response.py
+++ ex_12/request_response.py
@@ -16,18 +16,20 @@ print('>>>> Connection successful')
 
 # Sending a GET request. The 2nd part includes complete url-directory of the file.
 # The 3rd part of the string described the prototcol version + two carriage return & new line characters similar to telnet
-doc_cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode() # Encoding the sent data
+doc_cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode() # Encoding the sent data (UNICODE to UTF-8)
 my_socket.send(doc_cmd)
 print('>>>> Request sent')
 
 # Receiving the required file data
 #count = 0
+lines = str()
 while True:
     data = my_socket.recv(512) # 512 is the buffer_size. At a time the socket receives 512 characters
     if len(data) < 1 : break
     #count += 1
     #print('>>>> Requect cycle number:', count)
-    print(data.decode())
+    lines = lines + data.decode() # Converting UTF-8 to UNICODE for python
+print(lines)
 print('>>>> Data extracted successfully')
 
 # Closing the connection
