d431eebf944f99ed53c0cee0b9cae5e0468ac8bb
kaushik2000ksk <kaushik2000ksk@gmail.com>
2020-09-13 15:37:30 +0530

    Asssignment

--- ex_14/geojson.py
+++ ex_14/geojson.py
@@ -1,34 +1,53 @@
 # Service oriented json with google geomaps API
 
-import json
 import urllib.request, urllib.parse, urllib.error
+import json
+import ssl
 
-service_url = "https://maps.googleapis.com/maps/api/geocode/json?"
+api_key = False
+# If you have a Google Places API key, enter it here
+# api_key = 'AIzaSy___IDByT70'
+# https://developers.google.com/maps/documentation/geocoding/intro
 
-while True :
-    address = input("Enter the address: ")
-    if len(address) < 1 : break
+if api_key is False:
+    api_key = 42
+    serviceurl = 'http://py4e-data.dr-chuck.net/json?'
+else :
+    serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
 
-    url = service_url + urllib.parse.urlencode( {"address" : address} ) # Use urllib.parse.urlencode() to add params[address]
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
-    # Note Google is increasingly requiring API keys for this API 
-    print("Retrieving:", url)
-    url_handle = urllib.request.urlopen(url) # Get url handle using urllib.request.urlopen()
-    data = url_handle.read().decode() # Decode to convert UTF-8 to UNICODE
-    print("Retrieved", len(data), "characters")
+while True:
+    address = input('Enter location: ')
+    if len(address) < 1: break
+
+    parms = dict()
+    parms['address'] = address
+    if api_key is not False: parms['key'] = api_key
+    url = serviceurl + urllib.parse.urlencode(parms)
+
+    print('Retrieving', url)
+    uh = urllib.request.urlopen(url, context=ctx)
+    data = uh.read().decode()
+    print('Retrieved', len(data), 'characters')
 
     try:
         js = json.loads(data)
     except:
         js = None
 
-    if not js or 'status' not in js or js['status'] != 'OK' :
-        print("===Failed to retrieve data===")
+    if not js or 'status' not in js or js['status'] != 'OK':
+        print('==== Failure To Retrieve ====')
         print(data)
         continue
+
+    print(json.dumps(js, indent=4))
 
     lat = js['results'][0]['geometry']['location']['lat']
     lng = js['results'][0]['geometry']['location']['lng']
     print('lat', lat, 'lng', lng)
     location = js['results'][0]['formatted_address']
-    print('Location:', location)
+    print(location)
