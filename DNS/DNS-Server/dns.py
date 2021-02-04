502a095e5f69fe0f44bb55c37ddc2e7671a455de
Kaushik_L <kaushik2000ksk@gmail.com>
2021-02-04 20:36:00 +0530

    Create your own DNS server

---
+++ DNS/DNS-Server/dns.py
@@ -0,0 +1,319 @@
+'''
+IETC RFC 1035 (https://tools.ietf.org/html/rfc1035) - 3. DOMAIN NAME SPACE AND RR DEFINITIONS
+
+UDP messages    512 octets or less
+
+The header contains the following fields:
+
+                                    1  1  1  1  1  1
+      0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                      ID                       |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |QR|   Opcode  |AA|TC|RD|RA|   Z    |   RCODE   |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                    QDCOUNT                    |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                    ANCOUNT                    |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                    NSCOUNT                    |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                    ARCOUNT                    |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+
+where:
+
+ID              A 16 bit identifier assigned by the program that
+                generates any kind of query.  This identifier is copied
+                the corresponding reply and can be used by the requester
+                to match up replies to outstanding queries.
+
+QR              A one bit field that specifies whether this message is a
+                query (0), or a response (1).
+
+OPCODE          A four bit field that specifies kind of query in this
+                message.  This value is set by the originator of a query
+                and copied into the response.  The values are:
+
+                0               a standard query (QUERY)
+
+                1               an inverse query (IQUERY)
+
+                2               a server status request (STATUS)
+
+                3-15            reserved for future use
+
+AA              Authoritative Answer - this bit is valid in responses,
+                and specifies that the responding name server is an
+                authority for the domain name in question section.
+
+                Note that the contents of the answer section may have
+                multiple owner names because of aliases.  The AA bit
+
+
+
+Mockapetris                                                    [Page 26]
+ 
+RFC 1035        Domain Implementation and Specification    November 1987
+
+
+                corresponds to the name which matches the query name, or
+                the first owner name in the answer section.
+
+TC              TrunCation - specifies that this message was truncated
+                due to length greater than that permitted on the
+                transmission channel.
+
+RD              Recursion Desired - this bit may be set in a query and
+                is copied into the response.  If RD is set, it directs
+                the name server to pursue the query recursively.
+                Recursive query support is optional.
+
+RA              Recursion Available - this be is set or cleared in a
+                response, and denotes whether recursive query support is
+                available in the name server.
+
+Z               Reserved for future use.  Must be zero in all queries
+                and responses.
+
+RCODE           Response code - this 4 bit field is set as part of
+                responses.  The values have the following
+                interpretation:
+
+                0               No error condition
+
+                1               Format error - The name server was
+                                unable to interpret the query.
+
+                2               Server failure - The name server was
+                                unable to process this query due to a
+                                problem with the name server.
+
+                3               Name Error - Meaningful only for
+                                responses from an authoritative name
+                                server, this code signifies that the
+                                domain name referenced in the query does
+                                not exist.
+
+                4               Not Implemented - The name server does
+                                not support the requested kind of query.
+
+                5               Refused - The name server refuses to
+                                perform the specified operation for
+                                policy reasons.  For example, a name
+                                server may not wish to provide the
+                                information to the particular requester,
+                                or a name server may not wish to perform
+                                a particular operation (e.g., zone
+                                transfer) for particular data.
+                6-15            Reserved for future use.
+
+QDCOUNT         an unsigned 16 bit integer specifying the number of
+                entries in the question section.
+
+ANCOUNT         an unsigned 16 bit integer specifying the number of
+                resource records in the answer section.
+
+NSCOUNT         an unsigned 16 bit integer specifying the number of name
+                server resource records in the authority records
+                section.
+
+ARCOUNT         an unsigned 16 bit integer specifying the number of
+                resource records in the additional records section.
+
+Message compression
+
+In order to reduce the size of messages, the domain system utilizes a
+compression scheme which eliminates the repetition of domain names in a
+message.  In this scheme, an entire domain name or a list of labels at
+the end of a domain name is replaced with a pointer to a prior occurance
+of the same name.
+
+The pointer takes the form of a two octet sequence:
+
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    | 1  1|                OFFSET                   |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+
+The first two bits are ones.  This allows a pointer to be distinguished
+from a label, since the label must begin with two zero bits because
+labels are restricted to 63 octets or less.  (The 10 and 01 combinations
+are reserved for future use.)  The OFFSET field specifies an offset from
+the start of the message (i.e., the first octet of the ID field in the
+domain header).  A zero offset specifies the first byte of the ID field,
+etc.
+
+'''
+
+import socket, glob, json
+
+port = 53 # DNS runs always on port 53
+ip = '127.0.0.1' # This is the IP in which the local DNS server runs
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # AF_INET = IPv4 protocol & SOCK_DGRAM = Used to run in UDP
+sock.bind((ip, port))
+
+def load_zones():
+    # Loads all the zone files under 'zones/*.zone' path and saves them in a dictionary with the key as domain name
+    jsonzone = {}
+    zonefiles = glob.glob('zones/*.zone')
+
+    for zone in zonefiles:
+        with open(zone) as zonedata:
+            data = json.load(zonedata)
+            zonename = data["$origin"]
+            jsonzone[zonename] = data
+    return jsonzone
+
+zonedata = load_zones() # Contains dictionary with all zone files having 'domain' as the key
+
+def getflags(flags):
+    # Method to set flags for the header
+    byte1 = bytes(flags[:1])
+    byte2 = bytes(flags[1:2])
+
+    rflags = ''
+
+    QR = '1'
+
+    OPCODE = ''
+    for bit in range(1,5):
+        OPCODE += str(ord(byte1)&(1<<bit))
+
+    AA = '1'
+
+    TC = '0'
+
+    RD = '0'
+
+    # Byte 2
+
+    RA = '0'
+
+    Z = '000'
+
+    RCODE = '0000'
+
+    return int(QR+OPCODE+AA+TC+RD, 2).to_bytes(1, byteorder='big')+int(RA+Z+RCODE, 2).to_bytes(1, byteorder='big')
+
+def getquestiondomain(data):
+
+    state = 0
+    expectedlength = 0
+    domainstring = ''
+    domainparts = []
+    x = 0
+    y = 0
+    for byte in data:
+        if state == 1:
+            if byte != 0:
+                domainstring += chr(byte)
+            x += 1
+            if x == expectedlength:
+                domainparts.append(domainstring)
+                domainstring = ''
+                state = 0
+                x = 0
+            if byte == 0:
+                domainparts.append(domainstring)
+                break
+        else:
+            state = 1
+            expectedlength = byte
+        y += 1
+
+    questiontype = data[y:y+2]
+
+    return (domainparts, questiontype)
+
+def getzone(domain):
+    global zonedata # zonedata is obatained from load_zones() method
+
+    zone_name = '.'.join(domain)
+    return zonedata[zone_name]
+
+def getrecs(data): # Funtion to get the records of the particular domain from its particular zone file 
+    domain, questiontype = getquestiondomain(data)
+    qt = ''
+    if questiontype == b'\x00\x01':
+        qt = 'a'
+
+    zone = getzone(domain)
+
+    return (zone[qt], qt, domain) # Returs tuple containing file records in Json format (records under 'a' names, 'a', domain list)
+
+def buildquestion(domainname, rectype):
+    qbytes = b'' # Query bytes come after the header bytes (Consists of 'domain name', 'domain type' and 'domain class')
+
+    for part in domainname:
+        length = len(part)
+        qbytes += bytes([length])
+
+        for char in part:
+            qbytes += ord(char).to_bytes(1, byteorder='big') # ord : Converts ASCII to decimal in python
+
+    if rectype == 'a':
+        qbytes += (1).to_bytes(2, byteorder='big') # Record type 'a' : Hex value - "00 01" - 2 bytes
+
+    qbytes += (1).to_bytes(2, byteorder='big') # Class type 'IN'(Internet) : Hex value - "00 01" -2 bytes
+
+    return qbytes
+
+def rectobytes(domainname, rectype, recttl, recval):
+
+    rbytes = b'\xc0\x0c' # Using message compression here
+
+    if rectype == 'a':
+        rbytes = rbytes + bytes([0]) + bytes([1]) # Appending the type 'a' byte - '00 01' - 2 bytes
+
+    rbytes = rbytes + bytes([0]) + bytes([1]) # Appending the class bytes 'IN' - '00 01' - 2 bytes
+
+    rbytes += int(recttl).to_bytes(4, byteorder='big') # Appending the TTL value - 4 bytes 
+
+    if rectype == 'a':
+        rbytes = rbytes + bytes([0]) + bytes([4]) # Appending record length - '00 04' - Maximum IPv4 value that can be stored is 0 to 255 i.e. 256 values i.e. 4-bit value - 2 bytes
+
+        for part in recval.split('.'): # Splitting the ip address and converting into bytes (255.255.255.255)
+            rbytes += bytes([int(part)])
+    return rbytes
+
+def buildresponse(data):
+
+    # Transaction ID
+    TransactionID = data[:2]
+
+    # Get the flags
+    Flags = getflags(data[2:4])
+
+    # Question Count
+    QDCOUNT = b'\x00\x01'
+
+    # Answer Count
+    ANCOUNT = len(getrecs(data[12:])[0]).to_bytes(2, byteorder='big')
+
+    # Nameserver Count
+    NSCOUNT = (0).to_bytes(2, byteorder='big')
+
+    # Additonal Count
+    ARCOUNT = (0).to_bytes(2, byteorder='big')
+
+    dnsheader = TransactionID+Flags+QDCOUNT+ANCOUNT+NSCOUNT+ARCOUNT
+
+    # Create DNS body
+    dnsbody = b''
+
+    # Get answer for query
+    records, rectype, domainname = getrecs(data[12:])
+
+    dnsquestion = buildquestion(domainname, rectype)
+
+    for record in records:
+        dnsbody += rectobytes(domainname, rectype, record["ttl"], record["value"]) # For each 'a' record answer we have to send a 'type', 'class', 'TTL' & 'IP' for the actual answers
+
+    return dnsheader + dnsquestion + dnsbody
+
+
+while 1:
+    data, addr = sock.recvfrom(512)
+    r = buildresponse(data)
+    sock.sendto(r, addr)
